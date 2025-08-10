89793b2338a1a2d8f938d4649c545e2401380bd5
Mustafa Hocaoğlu <mustafaozgurh@gmail.com>
2025-08-10 23:29:07 +0300

    Implemented main entry point for the program

--- coffee_order/src/Main.java
+++ coffee_order/src/Main.java
@@ -1,5 +1,23 @@
+import service.MenuService;
+import service.OrderService;
+import service.FormattingService;
+import io.ConsoleInputReader;
+import io.ConsoleOutputWriter;
+import io.ConsoleUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Instantiate services
+        MenuService menuService = new MenuService();
+        OrderService orderService = new OrderService();
+        FormattingService formattingService = new FormattingService();
+
+        // Set up I/O
+        ConsoleInputReader inputReader = new ConsoleInputReader();
+        ConsoleOutputWriter outputWriter = new ConsoleOutputWriter();
+
+        // Create UI and run
+        ConsoleUI ui = new ConsoleUI(menuService, orderService, formattingService, inputReader, outputWriter);
+        ui.run();
     }
 }
